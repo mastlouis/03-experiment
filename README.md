a4ce9f1e338666f39a22ee59281bae11ff10bea3
imogencs <Imogen.ics1@gmail.com>
2021-03-05 11:33:29 -0500

    putting dots

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Assignment 3 - Replicating a Classic Experiment
 Sources
 ---
 https://www.d3-graph-gallery.com/graph/barplot_basic.html
+https://bl.ocks.org/arpitnarechania/027e163073864ef2ac4ceb5c2c0bf616
+
--- barcharts.js
+++ barcharts.js
@@ -1,70 +1,71 @@
-console.log("hi imo :)")
 
 var margin = { top: 20, right: window.innerWidth / 10, bottom: 80, left: window.innerWidth / 10 },
-    width = window.innerWidth - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+  width = window.innerWidth - margin.left - margin.right,
+  height = 600 - margin.top - margin.bottom;
 
 var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
-// svg.append('rect')
-//     .style("fill", "none")
-//     .style("pointer-events", "all")
-//     .attr('width', width)
-//     .attr('height', height)
-//     .on('mousemove', function (event) {
-//         crosshairs(event)
-//     })
-//     .attr('id', 'background')
+d3.csv("https://raw.githubusercontent.com/mastlouis/03-experiment/Pie-Charts/data.csv").then(function (data) {
+  let numBars = 10
+  let row = 0
+  let markedBar1 = 0
+  let markedBar2 = 0
+  let max = 25
 
-// axes
-// var x = d3.scaleTime()
-//     .range([0, width])
-//     .domain([1500, 5000]);
-// var y = d3.scaleLinear()
-//     .range([height, 0])
-//     .domain([5, 50]);
-// svg.append("g")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(d3.axisBottom(x));
-// svg.append("g")
-//     .call(d3.axisLeft(y));
+  d3.csv("./dots.csv").then( function (dots) {
+    console.log(dots)
+    markedBar1 = dots[row][0]
+    markedBar2 = dots[row][1]
+    
+})
+  // X axis
+  var x = d3.scaleBand()
+    .range([0, width])
+    .domain(data.map(function (d) { return d.columns; }))
+    .padding(0.2);
 
-// from source:
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, max])
+    .range([height, 0]);
 
-// X axis
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Country; }))
-  .padding(0.2);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+  // plain lines for axes - no ticks or numbers 
+  svg.append('line')
+    .attr('x1', 0)
+    .attr('y1', height)
+    .attr('x2', 0)
+    .attr('y2', 0)
+    .attr('stroke', 'black')
+    .attr('id', 'yAxis')
+  svg.append('line')
+    .attr('x1', 0)
+    .attr('y1', height)
+    .attr('x2', width)
+    .attr('y2', height)
+    .attr('stroke', 'black')
+    .attr('id', 'xAxis')
 
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, 13000])
-  .range([ height, 0]);
-svg.append("g")
-  .call(d3.axisLeft(y));
-
-// Bars
-svg.selectAll("mybar")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return x(d.Country); })
-    .attr("y", function(d) { return y(d.Value); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d.Value); })
-    .attr("fill", "#69b3a2")
-
+  // Bars
+  let interval = width / numBars / 10
+  let barWidth = width / numBars / 5 * 4
+  for (let i = 0; i < numBars; i++) {
+    svg.append('rect')
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('x', i * (interval * 2 + barWidth) + interval)
+      .attr('y', y(data[row][i]))
+      .attr('height', height - y(data[row][i]))
+      .attr('width', barWidth)
+    if (i == markedBar1 || i == markedBar2) {
+      svg.append('circle')
+        .attr('r', barWidth / 8)
+        .attr('cy', height - interval * 2)
+        .attr('cx', i * (interval * 2 + barWidth) + interval + barWidth / 2)
+    }
+  }
 })
