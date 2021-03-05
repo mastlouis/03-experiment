a828904100fd2cb84c30d9cef232d61bf9b0f7cc
imogencs <Imogen.ics1@gmail.com>
2021-03-05 13:07:06 -0500

    spiral barchart needs dots

--- barcharts.js
+++ barcharts.js
@@ -10,62 +10,59 @@ var svg = d3.select("body").append("svg")
   .attr("transform",
     "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("https://raw.githubusercontent.com/mastlouis/03-experiment/Pie-Charts/data.csv").then(function (data) {
+d3.csv("https://raw.githubusercontent.com/mastlouis/03-experiment/Pie-Charts/data.csv", function (data) {
   let numBars = 10
-  let row = 0
-  let markedBar1 = 0
-  let markedBar2 = 0
+  let row = 1
   let max = 25
 
-  d3.csv("./dots.csv").then( function (dots) {
-    console.log(dots)
-    markedBar1 = dots[row][0]
-    markedBar2 = dots[row][1]
-    
-})
-  // X axis
-  var x = d3.scaleBand()
-    .range([0, width])
-    .domain(data.map(function (d) { return d.columns; }))
-    .padding(0.2);
+  d3.csv("https://raw.githubusercontent.com/mastlouis/03-experiment/Bar-Charts/dots.csv", function (dots) {
+    let markedBar1 = dots[row][0]
+    let markedBar2 = dots[row][1]
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, max])
-    .range([height, 0]);
+    // X axis
+    var x = d3.scaleBand()
+      .range([0, width])
+      .domain(data.map(function (d) { return d.columns; }))
+      .padding(0.2);
 
-  // plain lines for axes - no ticks or numbers 
-  svg.append('line')
-    .attr('x1', 0)
-    .attr('y1', height)
-    .attr('x2', 0)
-    .attr('y2', 0)
-    .attr('stroke', 'black')
-    .attr('id', 'yAxis')
-  svg.append('line')
-    .attr('x1', 0)
-    .attr('y1', height)
-    .attr('x2', width)
-    .attr('y2', height)
-    .attr('stroke', 'black')
-    .attr('id', 'xAxis')
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, max])
+      .range([height, 0]);
 
-  // Bars
-  let interval = width / numBars / 10
-  let barWidth = width / numBars / 5 * 4
-  for (let i = 0; i < numBars; i++) {
-    svg.append('rect')
-      .attr('fill', 'none')
+    // plain lines for axes - no ticks or numbers 
+    svg.append('line')
+      .attr('x1', 0)
+      .attr('y1', height)
+      .attr('x2', 0)
+      .attr('y2', 0)
       .attr('stroke', 'black')
-      .attr('x', i * (interval * 2 + barWidth) + interval)
-      .attr('y', y(data[row][i]))
-      .attr('height', height - y(data[row][i]))
-      .attr('width', barWidth)
-    if (i == markedBar1 || i == markedBar2) {
-      svg.append('circle')
-        .attr('r', barWidth / 8)
-        .attr('cy', height - interval * 2)
-        .attr('cx', i * (interval * 2 + barWidth) + interval + barWidth / 2)
+      .attr('id', 'yAxis')
+    svg.append('line')
+      .attr('x1', 0)
+      .attr('y1', height)
+      .attr('x2', width)
+      .attr('y2', height)
+      .attr('stroke', 'black')
+      .attr('id', 'xAxis')
+
+    // Bars
+    let interval = width / numBars / 10
+    let barWidth = width / numBars / 5 * 4
+    for (let i = 0; i < numBars; i++) {
+      svg.append('rect')
+        .attr('fill', 'none')
+        .attr('stroke', 'black')
+        .attr('x', i * (interval * 2 + barWidth) + interval)
+        .attr('y', y(data[row][i]))
+        .attr('height', height - y(data[row][i]))
+        .attr('width', barWidth)
+      if (i == markedBar1 || i == markedBar2) {
+        svg.append('circle')
+          .attr('r', barWidth / 8)
+          .attr('cy', height - interval * 2)
+          .attr('cx', i * (interval * 2 + barWidth) + interval + barWidth / 2)
+      }
     }
-  }
+  })
 })
--- spiral-barcharts.js
+++ spiral-barcharts.js
@@ -11,107 +11,182 @@ d3.csv("https://raw.githubusercontent.com/mastlouis/03-experiment/Pie-Charts/dat
     end = 2.25,
     numSpirals = 1.2,
     margin = { top: 50, bottom: 50, left: 50, right: 50 };
+  let row = 0
 
-  var theta = function (r) {
-    return numSpirals * Math.PI * r;
-  };
+  d3.csv("https://raw.githubusercontent.com/mastlouis/03-experiment/Bar-Charts/dots.csv", function (dots) {
+    let markedBar1 = dots[row][0]
+    let markedBar2 = dots[row][1]
 
-  // used to assign nodes color by group
-  var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var theta = function (r) {
+      return numSpirals * Math.PI * r;
+    };
 
-  var r = d3.min([width, height]) / 2 - 40;
+    // used to assign nodes color by group
+    var r = d3.min([width, height]) / 2 - 40;
 
-  var radius = d3.scaleLinear()
-    .domain([start, end])
-    .range([40, r]);
+    var radius = d3.scaleLinear()
+      .domain([start, end])
+      .range([40, r]);
 
-  var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.left + margin.right)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.right + margin.left)
+      .attr("height", height + margin.left + margin.right)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var points = d3.range(start, end + 0.001, (end - start) / 1000);
+    var points = d3.range(start, end + 0.001, (end - start) / 1000);
 
-  var spiral = d3.radialLine()
-    .curve(d3.curveCardinal)
-    .angle(theta)
-    .radius(radius);
+    var spiral = d3.radialLine()
+      .curve(d3.curveCardinal)
+      .angle(theta)
+      .radius(radius);
 
-  var path = svg.append("path")
-    .datum(points)
-    .attr("id", "spiral")
-    .attr("d", spiral)
-    .style("fill", "none")
-    .style("stroke", "none");
+    var path = svg.append("path")
+      .datum(points)
+      .attr("id", "spiral")
+      .attr("d", spiral)
+      .style("fill", "none")
+      .style("stroke", "none");
 
-  var spiralLength = path.node().getTotalLength(),
-    N = 20,
-    barWidth = (spiralLength / N) - 1;
-  var someData = [];
-  for (var i = 0; i < N; i++) {
-    var currentDate = new Date();
-    currentDate.setDate(currentDate.getDate() + i);
-    someData.push({
-      date: currentDate,
-      value: Math.random(),
-      group: currentDate.getMonth()
-    });
-  }
+    var spiralLength = path.node().getTotalLength(),
+      N = 20,
+      barWidth = (spiralLength / N) - 1;
 
-  var timeScale = d3.scaleTime()
-    .domain(d3.extent(someData, function (d) {
-      return d.date;
-    }))
-    .range([0, spiralLength]);
+    var someData = [];
+    for (var i = 0; i < N; i++) {
+      var currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() + i);
+      someData.push({
+        date: currentDate,
+        value: data[row][i],
+        group: currentDate.getMonth()
+      });
+    }
+    var timeScale = d3.scaleTime()
+      .domain(d3.extent(someData, function (d) {
+        return d.date;
+      }))
+      .range([0, spiralLength]);
 
-  // yScale for the bar height
-  var yScale = d3.scaleLinear()
-    .domain([0, d3.max(someData, function (d) {
-      return d.value;
-    })])
-    .range([0, (r / numSpirals) - 30]);
+    // yScale for the bar height
+    var yScale = d3.scaleLinear()
+      .domain([0, d3.max(someData, function (d) {
+        return d.value;
+      })])
+      .range([0, (r / numSpirals) - 30]);
 
-  svg.selectAll("rect")
-    .data(someData)
-    .enter()
-    .append("rect")
-    .attr("x", function (d, i) {
+    // for (let i = 0; i < N; i++) {
+    //   // d = data[row]
+    //   svg.append('rect')
 
-      var linePer = timeScale(d.date),
-        posOnLine = path.node().getPointAtLength(linePer),
-        angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+    //     .attr("x", function (d, i) {
+    //       d = data[row]
+    //       // currentDate.setDate(currentDate.getDate() + i);
+    //       var linePer = timeScale(currentDate.setDate(currentDate.getDate() + i)),
+    //         posOnLine = path.node().getPointAtLength(linePer),
+    //         angleOnLine = path.node().getPointAtLength(linePer - barWidth);
 
-      d.linePer = linePer; // % distance are on the spiral
-      d.x = posOnLine.x; // x postion on the spiral
-      d.y = posOnLine.y; // y position on the spiral
+    //       d.linePer = linePer; // % distance are on the spiral
+    //       d.x = posOnLine.x; // x postion on the spiral
+    //       d.y = posOnLine.y; // y position on the spiral
 
-      d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+    //       d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
 
-      return d.x;
-    })
-    .attr("y", function (d) {
-      return d.y;
-    })
-    .attr("width", function (d) {
-      return barWidth;
-    })
-    .attr("height", function (d) {
-      return yScale(d.value);
-    })
-    .style("fill", 'none')
-    .style("stroke", "black")
-    .attr("transform", function (d) {
-      return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
-    });
+    //       return d.x;
+    //     })
+    //     .attr("y", function (d) {
+    //       d = data[row]
+    //       return d.y;
+    //     })
+    //     .attr("width", function (d) {
+    //       d = data[row]
+    //       return barWidth;
+    //     })
+    //     .attr("height", function (d) {
+    //       d = data[row]
+    //       console
+    //       return yScale(d[i]);
+    //     })
+    //     .style("fill", 'none')
+    //     .style("stroke", "black")
+    //     .attr("transform", function (d) {
+    //       d = data[row]
+    //       return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
+    //     });
 
-  var tooltip = d3.select("#chart")
-    .append('div')
-    .attr('class', 'tooltip');
+    // }
+    // svg.selectAll("rect")
+    //   .data(someData)
+    //   .enter()
+    //   .append("rect")
+    //   .attr("x", function (d, i) {
 
-  tooltip.append('div')
-    .attr('class', 'date');
-  tooltip.append('div')
-    .attr('class', 'value');
+    //     var linePer = timeScale(currentDate.setDate(currentDate.getDate() + i)),
+    //       posOnLine = path.node().getPointAtLength(linePer),
+    //       angleOnLine = path.node().getPointAtLength(linePer - barWidth);
 
+    //     d.linePer = linePer; // % distance are on the spiral
+    //     d.x = posOnLine.x; // x postion on the spiral
+    //     d.y = posOnLine.y; // y position on the spiral
+    //     d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+
+    //     return d.x;
+    //   })
+    //   .attr("y", function (d) {
+    //     return d.y;
+    //   })
+    //   .attr("width", function (d) {
+    //     return barWidth;
+    //   })
+    //   .attr("height", function (d, i) {
+    //     return yScale(d[i]);
+    //   })
+    //   .style("fill", 'none')
+    //   .style("stroke", "black")
+    //   .attr("transform", function (d) {
+    //     return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
+    //   });
+    svg.selectAll("rect")
+      .data(someData)
+      .enter()
+      .append("rect")
+      .attr("x", function (d, i) {
+
+        var linePer = timeScale(d.date),
+          posOnLine = path.node().getPointAtLength(linePer),
+          angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+
+        d.linePer = linePer; // % distance are on the spiral
+        d.x = posOnLine.x; // x postion on the spiral
+        d.y = posOnLine.y; // y position on the spiral
+
+        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+
+        return d.x;
+      })
+      .attr("y", function (d) {
+        return d.y;
+      })
+      .attr("width", function (d) {
+        return barWidth/2;
+      })
+      .attr("height", function (d) {
+        return yScale(d.value)/2;
+      })
+      .style("fill", 'none')
+      .style("stroke", "black")
+      .attr("transform", function (d) {
+        return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
+      });
+
+    var tooltip = d3.select("#chart")
+      .append('div')
+      .attr('class', 'tooltip');
+
+    tooltip.append('div')
+      .attr('class', 'date');
+    tooltip.append('div')
+      .attr('class', 'value');
+
+  })
 })
